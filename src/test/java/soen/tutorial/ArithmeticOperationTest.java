552c143b0ff078662ea96fe705d46b5f2f951654
David Ballard <108775943+dbal0503@users.noreply.github.com>
2025-04-07 20:47:02 -0400

    Update ArithmeticOperationTest.java

--- src/test/java/soen/tutorial/ArithmeticOperationTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationTest.java
@@ -14,8 +14,8 @@ public class ArithmeticOperationTest {
 		ArithmeticOperation operations = new ArithmeticOperation();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-		assertEquals(expected, actual);
-
+		//assertEquals(expected, actual);
+		fail("must fail");
 	}
     @Test
     public void testSub()
